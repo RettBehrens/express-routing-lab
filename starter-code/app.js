0d68bdbf1b8e5ce27274f8e8293ff55855c94ef4
RettBehrens <everettbehrens@gmail.com>
2017-07-21 07:27:16 -0600

    completes hw

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,17 +1,21 @@
 //Did you use npm install to
 //add all these packages
 //to our project?
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 
 // How do we 'require' the candyRouter file?
-var candyRouter;
+const candyRouter = require('./candyRouter.js');
 
 app.use(bodyParser.json());
 
 //How do we redirect the /candies path
 //through our candyRouter?
 //Hint: you need app.use
+app.use('/candies', candyRouter);
 
-app.listen(3000);
+app.listen(port, function() {
+	console.log('Server starter on ', port);
+});
--- starter-code/candyRouter.js
+++ starter-code/candyRouter.js
@@ -1,17 +1,65 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-router = express.Router();
-
-var candies;
+const express = require('express');
+const bodyParser = require('body-parser');
+const candyRouter = express.Router();
 
 //What would need to go into candies
 //in order to pass our first test?
 
-router.get('/', function(req,res) {
+// candies
+const candies = [
+	{ 
+		id: 1,
+		name: "Chewing Gum",
+		color: "Red"
+	},
+	{ 
+		id: 2,
+		name: "Pez",
+		color: "Green"
+	},
+	{ 
+		id: 3,
+		name: "Marshmallow",
+		color: "Pink"
+	},
+	{ 
+		id: 4,
+		name: "Candy Stick",
+		color: "Blue"
+	}
+];
+
+candyRouter.get('/', function(req, res) {
+	// INDEX
 	// What would go here? 
 	// Hint: we want all candies in JSON format
+	res.send(candies);
 });
 
 // Fill out the rest of the routes here
+candyRouter.get('/:id', function(req, res) {
+	// SHOW
+	res.send(candies[req.params.id-1]);
+});
 
-module.exports = router;
+candyRouter.post('/', function(req, res) {
+	// CREATE
+	candies.push(req.body);
+	console.log(req.body);
+	res.send(req.body);
+});
+
+candyRouter.put('/:id', function(req, res) {
+	// UPDATE//
+	candies[req.params.id-1] = req.body;
+	res.send(req.body);
+	//res.send(candies[req.params.id-1] = req.body);
+});
+
+candyRouter.delete('/:id', function(req, res) {
+	// DELETE
+	candies.splice(req.params.id-1, 1);
+	res.send(req.body);
+});
+
+module.exports = candyRouter;
